3f2904e315a234c6701d94d81065ee3658bc44e7
vbrignatz <vincentbrignatz@gmail.com>
2021-05-28 14:42:47 +0200

    wip

--- train_resnet.py
+++ train_resnet.py
@@ -256,7 +256,153 @@ def train_contrastive(args, generator, projection_head, dataloader_train, device
             # Testing the saved model
             if dataset_validation is not None:
                 logger.info('Model Evaluation')
-                test_res = score_utt_utt(generator, dataset_validation)
+                test_res = extract_and_score(generator, dataset_validation)
+                for veri_pair, res in test_res.items():
+                    eer = res['eer']
+                    logger.info(f'EER on {veri_pair}: {eer}')
+                    writer.add_scalar(f'{veri_pair}_EER', eer, iterations)
+                    if eer < best_eer[veri_pair]["eer"]:
+                        best_eer[veri_pair]["eer"] = eer
+                        best_eer[veri_pair]["ite"] = iterations
+                msg = ""
+                for veri, vals in best_eer.items():
+                    msg += f"\nBest score for {veri} is at iteration {vals['ite']} : {vals['eer']} eer"
+                logger.success(msg)
+            logger.info(f"Saved checkpoint at iteration {iterations}")
+
+    # Final model saving
+    for model, modelstr in [(generator, 'g'), (projection_head, 'c')]:
+        model.eval().cpu()
+        cp_filename = "final_{}_{}.pt".format(modelstr, iterations)
+        cp_model_path = args.model_dir / cp_filename
+        torch.save(model.state_dict(), cp_model_path)
+    logger.success(f'Training complete in {time.process_time()-start} seconds')
+
+
+@logger.catch
+def train_multitask(args, generator, classifier, projection_head, dataloader_train, device, dataset_validation=None):
+    # Tensorflow logger
+    writer = SummaryWriter(comment='_{}'.format(args.model_dir.name))
+
+    # Load the trained model if we continue from a checkpoint
+    start_iteration = 0
+    if args.checkpoint >= 0:
+        start_iteration = args.checkpoint
+        # for model, modelstr in [(generator, 'g'), (classifier, 'c')]:
+        #     model.load_state_dict(torch.load(args.checkpoints_dir / f'{modelstr}_{args.checkpoint}.pt'))
+        generator.load_state_dict(torch.load(args.checkpoints_dir / f'g_{args.checkpoint}.pt'))
+    
+    # elif args.checkpoint == -1:
+    #     start_iteration = max([int(filename.stem[2:]) for filename in args.checkpoints_dir.iterdir()])
+    #     for model, modelstr in [(generator, 'g'), (classifier, 'c')]:
+    #         model.load_state_dict(torch.load(args.checkpoints_dir / f'{modelstr}_{start_iteration}.pt'))
+
+    # Optimizer definition
+    optimizer = torch.optim.SGD([{'params': generator.parameters(), 'lr': args.generator_lr},
+                                 {'params': projection_head.parameters(), 'lr': args.classifier_lr},
+                                 {'params': classifier.parameters(), 'lr': args.classifier_lr}],
+                                momentum=args.momentum)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # multi GPU support :
+    if args.multi_gpu:
+        dpp_generator = nn.DataParallel(generator).to(device)
+    
+    if dataset_validation is not None:
+        best_eer = {v.name:{'eer':100, 'ite':-1} for v in dataset_validation.trials} # best eer of all iterations
+
+    start = time.process_time()
+    for iterations in range(start_iteration, args.num_iterations + 1):
+        # The current iteration is specified in the scheduler
+        # Reduce the learning rate by the given factor (args.scheduler_lambda)
+        if iterations in args.scheduler_steps:
+            for params in optimizer.param_groups:
+                params['lr'] *= args.scheduler_lambda
+            print(optimizer)
+
+        avg_loss = 0
+        for f1, f2, spk in dataloader_train:
+            f1 = f1.unsqueeze(1)
+            f2 = f2.unsqueeze(1)
+            features = torch.cat((f1, f2), dim=0).to(device)
+            target = torch.cat([spk, spk], dim=0).to(device)
+            # Creating embeddings
+            if args.multi_gpu:
+                embeds = dpp_generator(features)
+            else:
+                embeds = generator(features)
+
+            preds = classifier(embeds, target)
+
+            # Calc the loss
+            ce_loss = criterion(preds, target)
+            
+            embeds = projection_head(embeds)
+
+            # Labels calculation
+            labels = torch.cat([spk, spk], dim=0)
+            labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
+            labels = labels.to(device)
+
+            # Cosine calculation
+            embeds = F.normalize(embeds, dim=1)
+            similarity_matrix = torch.matmul(embeds, embeds.T)
+
+            # discard the main diagonal from both: labels and similarities matrix
+            mask = torch.eye(labels.shape[0], dtype=torch.bool).to(device)
+            labels = labels[~mask].view(labels.shape[0], -1)
+            similarity_matrix = similarity_matrix[~mask].view(similarity_matrix.shape[0], -1)
+
+            # select and combine multiple positives
+            positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
+
+            # select only the negatives the negatives
+            negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
+
+            logits = torch.cat([positives, negatives], dim=1)
+            labels = torch.zeros(logits.shape[0], dtype=torch.long).to(device)
+
+            # Calc the loss
+            contrast_loss = criterion(logits, labels)
+
+            # Backpropagation
+            loss = ce_loss + contrast_loss
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            avg_loss += loss.item()
+        
+        avg_loss /= len(dataloader_train)
+        # Write the loss in tensorflow
+        writer.add_scalar('Loss', avg_loss, iterations)
+
+        # loguru logging :
+        if iterations % args.log_interval == 0:
+            msg = "{}: {}: [{}/{}] \t C-Loss:{:.4f}, lr: {}, bs: {}".format(args.model_dir,
+                                                                            time.ctime(),
+                                                                            iterations,
+                                                                            args.num_iterations,
+                                                                            avg_loss,
+                                                                            get_lr(optimizer),
+                                                                            args.batch_size
+                                                                            )
+            logger.info(msg) 
+
+         # Saving checkpoint
+        if iterations % args.checkpoint_interval == 0:
+            
+            for model, modelstr in [(generator, 'g'), (projection_head, 'c')]:
+                model.eval().cpu()
+                cp_model_path = args.checkpoints_dir / f"{modelstr}_{iterations}.pt"
+                torch.save(model.state_dict(), cp_model_path)
+                model.to(device).train()
+
+            # Testing the saved model
+            if dataset_validation is not None:
+                logger.info('Model Evaluation')
+                test_res = extract_and_score(generator, dataset_validation)
                 for veri_pair, res in test_res.items():
                     eer = res['eer']
                     logger.info(f'EER on {veri_pair}: {eer}')
